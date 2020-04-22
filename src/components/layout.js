99aa9accf40149808ca48d4e00bb8900d68c838f
Valentin Bellamy <bellamyvalentin@gmail.com>
2020-04-22 17:48:15 +0200

    styles ready

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../static/fonts/fonts.css"
+import "../static/style/index.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
