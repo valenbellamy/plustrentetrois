94122b8307f6b9ef3c51673dccf1d05ac578c193
Valentin Bellamy <bellamyvalentin@gmail.com>
2020-05-19 14:55:37 +0200

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,11 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <Link to="/">Go to the homepage</Link>
+    <section className="error404">
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to="/">Go to the homepage</Link>
+    </section>
   </Layout>
 )
 
